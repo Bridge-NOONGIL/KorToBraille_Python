8a5b88b4ac4dcf12e7db1c42b489001ee6ef879a
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-09-03 23:51:13 +0900

    [FEAT] KorToBraille

--- KorToBraille.py
+++ KorToBraille.py
@@ -1,15 +1,3 @@
-
-# class KorToBraille:
-
-
-#     def __init__(self) -> None:
-#         pass
-
-
-
-# if __name__ == '__main__':
-#     print("Main.py")
-
 # 초성 리스트. 00 ~ 18
 CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 # 중성 리스트. 00 ~ 20
@@ -17,21 +5,29 @@ JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 
 # 종성 리스트. 00 ~ 27 + 1(1개 없음)
 JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 
-def getJamoFromOneSyllable(korean_word):
-    r = ""
-    for w in list(korean_word.strip()):
-        ## 영어인 경우 구분해서 작성함. 
-        if '가'<=w<='힣':
-            ## 588개 마다 초성이 바뀜. 
-            ch1 = (ord(w) - ord('가'))//588
-            ## 중성은 총 28가지 종류
-            ch2 = ((ord(w) - ord('가')) - (588*ch1)) // 28
-            ch3 = (ord(w) - ord('가')) - (588*ch1) - 28*ch2
-            # r_lst.append([CHOSUNG_LIST[ch1], JUNGSUNG_LIST[ch2], JONGSUNG_LIST[ch3]])
-            r += (CHOSUNG_LIST[ch1] + JUNGSUNG_LIST[ch2] + JONGSUNG_LIST[ch3])
-        else:
-            r += w
-    return r
-    
-print(getJamoFromOneSyllable("읷"))
-print("ㄳ"== "ㄳ")
+class KorToBraille:
+
+
+    def __init__(self) -> None:
+        pass
+
+    # [한글 분해] 자모음으로 분해해서 리턴하는 함수 (한글로 규칙을 파악해야할 때 사용)
+    def getJamoFromOneSyllable(self, korean_word):
+        r = ""
+        for w in list(korean_word.strip()):
+            ## 영어인 경우 구분해서 작성함. 
+            if '가'<=w<='힣':
+                ## 588개 마다 초성이 바뀜. 
+                ch1 = (ord(w) - ord('가'))//588
+                ## 중성은 총 28가지 종류
+                ch2 = ((ord(w) - ord('가')) - (588*ch1)) // 28
+                ch3 = (ord(w) - ord('가')) - (588*ch1) - 28*ch2
+                # r_lst.append([CHOSUNG_LIST[ch1], JUNGSUNG_LIST[ch2], JONGSUNG_LIST[ch3]])
+                r += (CHOSUNG_LIST[ch1] + JUNGSUNG_LIST[ch2] + JONGSUNG_LIST[ch3])
+            else:
+                r += w
+        return r
+
+if __name__ == '__main__':
+    b = KorToBraille().getJamoFromOneSyllable("자")
+    print(b)
