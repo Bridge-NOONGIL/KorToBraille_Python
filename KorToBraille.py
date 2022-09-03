81523162db3887bc263268108f9adb50cc44e5d0
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-09-03 23:56:28 +0900

    [FEAT] KorToBraille

--- KorToBraille.py
+++ KorToBraille.py
@@ -1,4 +1,7 @@
 # 초성 리스트. 00 ~ 18
+
+
+
 CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 # 중성 리스트. 00 ~ 20
 JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
@@ -27,6 +30,55 @@ class KorToBraille:
             else:
                 r += w
         return r
+
+    # [단어 번역] 주어진 "단어"를 자모음으로 분해해서 번역된 점자로 리턴하는 함수
+    def korWordToBraille(self, input: str):
+        
+        wordToTranslate = "" # 번역할 단어
+        
+        jamo = "" # 분해된 문장
+        braillejamo = "" # 분해된 점자 문장
+
+        wordToTranslate = self.korabbToBraille(input) # 약어 처리
+
+        for scalar in wordToTranslate:
+            # 자모음 분해
+            try:
+                jamo += self.getJamoFromOneSyllable(scalar)
+            except:
+                jamo += ""
+            
+            # 점자 번역
+            try:
+                braillejamo += self.getBrailleFromJamo(scalar)
+            except:
+                jamo += ""
+
+        return braillejamo
+
+
+    # [최종 번역] 주어진 문장을 단어로 decompose -> korWordToBraille로 각각 점자 단어로 변경 -> 점자 단어 compose (result)
+    def korTranslate(self, input: str):
+        result = ""
+        components = input.split( )
+
+        for word in components:
+            if word == "":
+                continue
+            
+            # (1) 숫자번역
+            # (2) 문장부호 번역
+            result += self.korWordToBraille(word) # (3) 한글번역
+
+            result += " "
+
+            # 모든 flag 초기화
+            flag_10 = False
+            flag_11 = False
+            flag_17 = False
+            isdigit_flag = False
+        
+        return result
 
 if __name__ == '__main__':
     b = KorToBraille().getJamoFromOneSyllable("자")
