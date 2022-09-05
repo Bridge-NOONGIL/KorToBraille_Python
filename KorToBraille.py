b49ebc0190ced417ed84d12f976785e6fb4a7d36
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-09-05 18:54:42 +0900

    [FEAT] getBrailleFromJamo

--- KorToBraille.py
+++ KorToBraille.py
@@ -1,6 +1,6 @@
+from data import kor_abb, kor_cho, kor_jung, kor_jong
+
 # 초성 리스트. 00 ~ 18
-
-
 
 CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 # 중성 리스트. 00 ~ 20
@@ -8,11 +8,37 @@ JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 
 # 종성 리스트. 00 ~ 27 + 1(1개 없음)
 JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 
-class KorToBraille:
+CHO = ["ㄱ","ㄲ","ㄴ","ㄷ","ㄸ","ㄹ","ㅁ","ㅂ","ㅃ","ㅅ","ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"]
+    
+JUNG = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ","ㅕ", "ㅖ", "ㅗ", "ㅘ","ㅙ", "ㅚ","ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ","ㅣ"]
+    
+JONG = ["","ㄱ","ㄲ","ㄳ","ㄴ","ㄵ","ㄶ","ㄷ","ㄹ","ㄺ","ㄻ","ㄼ","ㄽ","ㄾ","ㄿ","ㅀ","ㅁ","ㅂ","ㅄ","ㅅ","ㅆ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"]
+    
+JONG_DOUBLE = {"ㄲ":"ㄱㄱ","ㄳ":"ㄱㅅ","ㄵ":"ㄴㅈ","ㄶ":"ㄴㅎ","ㄺ":"ㄹㄱ","ㄻ":"ㄹㅁ","ㄼ":"ㄹㅂ","ㄽ":"ㄹㅅ","ㄾ":"ㄹㅌ", "ㄿ":"ㄹㅍ","ㅀ":"ㄹㅎ","ㅄ":"ㅂㅅ"}
 
+class KorToBraille:
 
     def __init__(self) -> None:
         pass
+
+    #  [약어 번역]
+    # 제18항: 다음이 단어들은 약어로 적어 나타낸다.
+    #  [붙임] 위에 제시된 말들의 뒤에 다른 음절이 붙어 쓰일 때에도 약어를 사용하여 적는다. ex. 그래서인지, 그러면서
+    #  [다만] 위에 제시된 말들의 앞에 다른 음절이 붙어 쓰일 때에는 약어를 사용하여 적지 않는다. ex. 쭈그리고, 찡그리고, 오그리고
+
+    def korabbToBraille(input: str):
+        result = input
+
+        for (key, value) in kor_abb.items():
+            if key in input: 
+                # [다만] 맨 앞에 오는 경우인지 확인
+                for i in input:
+                    if i == key[0]:
+                        result.replace(key, value)
+                    else:
+                        break
+        return result
+
 
     # [한글 분해] 자모음으로 분해해서 리턴하는 함수 (한글로 규칙을 파악해야할 때 사용)
     def getJamoFromOneSyllable(self, korean_word):
@@ -30,6 +56,23 @@ class KorToBraille:
             else:
                 r += w
         return r
+
+    # [점자 번역] 자모음으로 분해한 음절을 "점자"로 번역하여 리턴하는 함수 (한 음절씩 번역 처리(초성+중성+종성))
+    def getBrailleFromJamo(self, n):
+        if "가" <= n <= "힣":
+            try:
+                cho, jung, jong = self.getJamoFromOneSyllable(n)
+            except:
+                cho, jung = self.getJamoFromOneSyllable(n)
+                jong = ""
+            
+            # 제2항: ‘ᄋ’이 첫소리 자리에 쓰일 때에는 이를 표기하지 않는다.
+            if cho == "ㅇ":
+                cho = ""
+            
+            braille_cho = kor_cho[cho] # 초성 점자
+
+            # 중성
 
     # [단어 번역] 주어진 "단어"를 자모음으로 분해해서 번역된 점자로 리턴하는 함수
     def korWordToBraille(self, input: str):
---
+++ test.py
@@ -0,0 +1,4 @@
+for i in 'asd f':
+  print(i)
+
+print("가"< "돩"< "힣")
