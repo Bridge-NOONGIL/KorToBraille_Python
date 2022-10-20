ea58fac1b288625ddfd97bc2d0adb0ebd128a02e
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-10-21 01:16:58 +0900

    [FEAT] PunctuationFunc

--- src/KorToBraille/NumberFunc.py
+++ src/KorToBraille/NumberFunc.py
@@ -1,1 +1,38 @@
-    
+number_braille = "⠼"
+number_braille_dict = {"0":"⠚", "1":"⠁", "2":"⠃", "3":"⠉", "4":"⠙","5":"⠑", "6":"⠋", "7":"⠛", "8":"⠓", "9":"⠊"}
+
+hangul = ["ㄱ","ㄲ","ㄴ","ㄷ","ㄸ","ㄹ","ㅁ","ㅂ","ㅃ","ㅅ","ㅆ","ㅇ","ㅈ","ㅉ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"]
+number_punctuation_invalid_dict = {"~":"⠤⠤"} # 수표 효력 무효
+number_punctuation_valid_dict = {":":"⠐⠂", "-":"⠤", ".":"⠲", ",":"⠐", "·":"⠐⠆"} # 수표 효력 유효
+
+number_punctuation_valid = [":", "-", ".", ",", "·"]
+isdigit_flag = False
+
+CHOSUNG_LIST = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+# 중성 리스트. 00 ~ 20
+JUNGSUNG_LIST = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+# 종성 리스트. 00 ~ 27 + 1(1개 없음)
+JONGSUNG_LIST = [' ', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+def getJamoFromOneSyllable(korean_word):
+    r = ""
+    for w in list(korean_word.strip()):
+        ## 영어인 경우 구분해서 작성함. 
+        if '가'<=w<='힣':
+            ## 588개 마다 초성이 바뀜. 
+            ch1 = (ord(w) - ord('가'))//588
+            ## 중성은 총 28가지 종류
+            ch2 = ((ord(w) - ord('가')) - (588*ch1)) // 28
+            ch3 = (ord(w) - ord('가')) - (588*ch1) - 28*ch2
+            # r_lst.append([CHOSUNG_LIST[ch1], JUNGSUNG_LIST[ch2], JONGSUNG_LIST[ch3]])
+            r += (CHOSUNG_LIST[ch1] + JUNGSUNG_LIST[ch2] + JONGSUNG_LIST[ch3])
+        else:
+            r += w
+    return r
+
+def chosungCheck(word: str):
+  return getJamoFromOneSyllable(word)[0]
+
+def translateNumber(text: str):
+  result = ""
+  
---
+++ src/KorToBraille/PunctuationFunc.py
@@ -0,0 +1,31 @@
+punctuation_dict = {".": "⠲", "?": "⠦", "!": "⠖", ",": "⠐", "·": "⠐⠆", ":": "⠐⠂", ";":"⠰⠆", "(": "⠦⠄", ")":"⠠⠴", "{": "⠦⠂", "}": "⠐⠴", "[": "⠦⠆", "]": "⠰⠴", "-": "⠤", "~": "⠤⠤" }
+
+punctuation_list = [".", "?", "!", ",", "·", ":", ";", "(", ")", "{", "}", "[", "]", "-", "~"]
+quotes_kor_list = ["\"", "'"]
+quotes_list = ["⠦", "⠴", "⠠⠦", "⠴⠄"] # 여는 큰따옴표, 닫는 큰따옴표, 여는 작은따옴표, 닫는 작은따옴표
+
+open_flag = False # 따옴표 열고 닫는 flag
+
+def translatePunc(text: str):
+  result = ""
+  for i in range(len(text)):
+    if text[i] in quotes_kor_list:
+      if open_flag == False:
+        if text[i] == "'":
+          result += quotes_list[2] # 여는 작은따옴표
+        elif text[i] == "\"":
+          result += quotes_list[0] # 여는 큰 따옴표
+        open_flag = True
+      else:
+        if text[i] == "'":
+          result += quotes_list[3] # 닫는 작은따옴표
+        elif text[i] == "\"":
+          result += quotes_list[1] # 닫는 큰 따옴표
+        
+        open_flag = False
+    elif text[i] in punctuation_list:
+      result += punctuation_dict[text[i]]
+    else:
+      result += text[i]
+  
+  return result
